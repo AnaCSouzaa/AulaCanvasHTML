9c67aca3a25240e3182809e85a986220e94526c2
Ana Cecília Souza <anaceciams@gmail.com>
2022-04-26 16:17:33 -0300

    batman, coração e estrela

--- canvas.js
+++ canvas.js
@@ -33,13 +33,11 @@ for(var b = 0; b <= 30; b++){
     c.strokeStyle = "#" + randomColor; //desafio 3
     c.stroke();
 }
+
 //desenhar estrela 
 //https://www.tutorialspoint.com/How-to-draw-a-star-by-using-canvas-HTML5
-for(var e = 0; e <= 10; e++){ 
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     c.fillStyle = "#" +  randomColor;
-    dx = Math.floor(Math.random()*500+800) ; //desafio 2
-    dy = Math.floor(Math.random()*600) ; //desafio 2
     c.beginPath();
     c.moveTo(108, 0.0);
     c.lineTo(141, 70);
@@ -54,14 +52,11 @@ for(var e = 0; e <= 10; e++){
     c.lineTo(108, 0);
     c.closePath();
     c.fill();
-}    
+
 //desenhar coração
 //https://developer.mozilla.org/pt-BR/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
-for(var h = 0; h <= 10; h++){ 
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     // Exemplo de curvas de Bézier cúbicas
-    dx = Math.floor(Math.random()*500)
-    dy = Math.floor(Math.random()*600) ; 
     c.fillStyle = "#" +  randomColor;
     c.beginPath();
     c.moveTo(75,40);
@@ -72,31 +67,31 @@ for(var h = 0; h <= 10; h++){
     c.bezierCurveTo(130,62.5,130,25,100,25);
     c.bezierCurveTo(85,25,75,37,75,40);
     c.fill();
-}
+
 //desenhar batman
 //https://codepen.io/travissanon/pen/YEPwGM
-c.beginPath();
-c.moveTo(1000/5, 800/5);
-c.quadraticCurveTo(1000/5, 460/5, 1390/5, 650/5);
-c.quadraticCurveTo(1400/5, 490/5, 1650/5, 650/5);
-c.quadraticCurveTo(1400/5, 250/5, 1300/5, 250/5);
-c.lineTo(1275/5, 350/5);
-c.lineTo(1075/5, 400/5);
-c.lineTo(1060/5, 300/5);
-c.lineTo(1030/5, 360/5);
-c.lineTo(1000/5, 365/5);
-c.lineTo(970/5, 360/5);
-c.lineTo(940/5, 300/5);
-c.lineTo(925/5, 400/5);
-c.lineTo(725/5, 350/5);
-c.lineTo(700/5, 250/5);
-c.quadraticCurveTo(600/5, 250/5, 350/5, 650/5);
-c.quadraticCurveTo(600/5, 490/5, 590/5, 650/5);
-c.quadraticCurveTo(1000/5, 460/5, 1000/5, 800/5);
-c.lineWidth = 2;
-var randomColor = Math.floor(Math.random()*16777215).toString(16); 
-c.fillStyle = "#" + randomColor; 
-c.fill();
+    c.beginPath();
+    c.moveTo(1000/5, 800/5);
+    c.quadraticCurveTo(1000/5, 460/5, 1390/5, 650/5);
+    c.quadraticCurveTo(1400/5, 490/5, 1650/5, 650/5);
+    c.quadraticCurveTo(1400/5, 250/5, 1300/5, 250/5);
+    c.lineTo(1275/5, 350/5);
+    c.lineTo(1075/5, 400/5);
+    c.lineTo(1060/5, 300/5);
+    c.lineTo(1030/5, 360/5);
+    c.lineTo(1000/5, 365/5);
+    c.lineTo(970/5, 360/5);
+    c.lineTo(940/5, 300/5);
+    c.lineTo(925/5, 400/5);
+    c.lineTo(725/5, 350/5);
+    c.lineTo(700/5, 250/5);
+    c.quadraticCurveTo(600/5, 250/5, 350/5, 650/5);
+    c.quadraticCurveTo(600/5, 490/5, 590/5, 650/5);
+    c.quadraticCurveTo(1000/5, 460/5, 1000/5, 800/5);
+    c.lineWidth = 2;
+    var randomColor = Math.floor(Math.random()*16777215).toString(16); 
+    c.fillStyle = "#" + randomColor; 
+    c.fill();
 
 
 
